f3a223ea033d8db0a111323fa58898c9aa188354
jguese3 <jguese3@rrc.ca>
2026-01-26 23:26:53 -0600

    Feat: turned department input to select dropdown and refactored handleSubmit. Still doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { Employees } from './components/features/employees/Employees'
-import { Footer } from './components/layout/footer/Footer'
-import { Header } from './components/layout/header/Header'
-import { Form } from './components/features/form/Form'
+import { useState } from 'react';
+import { Employees } from './components/features/employees/Employees';
+import { Footer } from './components/layout/footer/Footer';
+import { Header } from './components/layout/header/Header';
+import { Form } from './components/features/form/Form';
+import departmentData from './data/departments';
+import type { Department } from './types/department';
 
 function App() {
+  const [departments, setDepartments] = useState<Department[]>(departmentData);
 
   return (
     <>
       <Header />
-      <Employees />
-      <Form />
+      <Employees/>
+      <Form 
+        departments={departments} 
+        updateDepartments={setDepartments} 
+      />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
