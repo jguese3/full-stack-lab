af3de40c19213dd4524e4bc63de3aeb885ded7f9
jguese3 <jguese3@rrc.ca>
2026-01-28 15:02:41 -0600

    Feat: Added organization data and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 import { Employees } from './components/features/employees/Employees';
+import { Footer } from './components/layout/footer/Footer';
+import { Header } from './components/layout/header/Header';
 import { Form } from './components/features/form/Form';
 import departmentData from './data/departments';
 import type { Department } from './types/department';
-import { Layout } from "./components/layout/Layout";
 
 function App() {
   const [departments, setDepartments] = useState<Department[]>(departmentData);
 
   return (
-<Routes>
-  <Route index element={<Layout />} />
-</Routes>
+    <>
+      <Header />
+      <Employees departments={departments}/>
+      <Form 
+        departments={departments} 
+        updateDepartments={setDepartments} 
+      />
+      <Footer />
+    </>
   );
 }
 
