513317b3932510072941c14d16634724e4dcdd25
Jarone Guese <151054906+jguese3@users.noreply.github.com>
2026-02-01 20:39:55 -0600

    Refactor App component to remove unused elements
    Removed Employees and Form components from the App.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import { Header } from './components/layout/header/Header';
 import { Form } from './components/features/form/Form';
 import departmentData from './data/departments';
 import type { Department } from './types/department';
-<<<<<<< HEAD
 import { Nav } from './components/layout/nav/Nav';
 import { OrganizationList } from './components/features/organizations/Organizations';
 import { organizationData } from './data/organization';
 import { Routes, Route } from 'react-router-dom';
-=======
->>>>>>> 29271c713ab2bf8439485582df51b7aa4536e1ed
 
 function App() {
   const [departments, setDepartments] = useState<Department[]>(departmentData);
@@ -19,7 +16,6 @@ function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <Nav />
       <Routes>
         <Route
@@ -39,20 +35,9 @@ function App() {
           element={<OrganizationList organization={organizationData}/>}
           />
       </Routes>
-=======
-      <Employees/>
-      <Form 
-        departments={departments} 
-        updateDepartments={setDepartments} 
-      />
->>>>>>> 29271c713ab2bf8439485582df51b7aa4536e1ed
       <Footer />
     </>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 29271c713ab2bf8439485582df51b7aa4536e1ed
